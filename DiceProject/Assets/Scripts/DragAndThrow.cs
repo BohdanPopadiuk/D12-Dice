4d95f86fe901cdd1f26b7184e68146b59b560eaa
Bohdan Popadiuk <bohdan.popadiuk.games@gmail.com>
2024-01-07 17:43:15 +0100

    PickUp Animation & Cursor visibility

--- DiceProject/Assets/Scripts/DragAndThrow.cs
+++ DiceProject/Assets/Scripts/DragAndThrow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class DragAndThrow : MonoBehaviour
@@ -8,17 +9,24 @@ public class DragAndThrow : MonoBehaviour
 
     public static Action DiceThrown;
     
-    private Camera _mainCamera;
-    private Rigidbody _selectedRigidbody;
     private readonly List<Vector3> _dragTracking = new List<Vector3>();
+    private Vector3 _diceOffset = new Vector3();
+    
+    private Rigidbody _selectedRigidbody;
+    private Camera _mainCamera;
+    private Tween _diceMoveTween;
     
     [SerializeField] private float throwMultiplier = 50000;
     [SerializeField] private float torqueMultiplier = 50000;
     [SerializeField] private float wallOffset = 2.0f;
+    [SerializeField] private float dicePosY = 3.0f;
+    [SerializeField] private float pickUpAnimDuration = 0.3f;
+    [SerializeField] private Ease pickUpEase = Ease.Linear;
+    
     [SerializeField] private Transform[] walls;// 0-left | 1-back | 2-right | 3-front
     [SerializeField] private LayerMask rayPanelLayerMask;
     [SerializeField] private LayerMask diceLayerMask;
-    
+     
     #endregion
     
     private void Start()
@@ -38,11 +46,15 @@ public class DragAndThrow : MonoBehaviour
     private void PickUp()
     {
         Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, diceLayerMask))
-        {
-            if (hit.collider == null) return;
-            _selectedRigidbody = hit.collider.gameObject.GetComponent<Rigidbody>();
-        }
+        Physics.Raycast(ray, out RaycastHit diceHit, float.MaxValue, diceLayerMask);
+        
+        if (diceHit.collider == null) return;
+        
+        Physics.Raycast(ray, out RaycastHit rayPanelHit, float.MaxValue, rayPanelLayerMask);
+        _diceOffset = rayPanelHit.point - diceHit.collider.transform.position; 
+        
+        _selectedRigidbody = diceHit.collider.gameObject.GetComponent<Rigidbody>();
+        _diceMoveTween = _selectedRigidbody.DOMoveY(dicePosY, pickUpAnimDuration).SetEase(pickUpEase);
     }
     
     private void Drag()
@@ -54,24 +66,29 @@ public class DragAndThrow : MonoBehaviour
         _selectedRigidbody.freezeRotation = true;
         if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, rayPanelLayerMask))
         {
-            float xPos = Mathf.Clamp(hit.point.x,
+            float xPos = Mathf.Clamp(hit.point.x - _diceOffset.x,
                 walls[0].position.x + wallOffset, walls[2].position.x - wallOffset);
-            float zPos = Mathf.Clamp(hit.point.z,
+            float zPos = Mathf.Clamp(hit.point.z - _diceOffset.z,
                 walls[3].position.z + wallOffset, walls[1].position.z - wallOffset);
             
+            Cursor.visible = Vector3.Distance(hit.point, new Vector3(xPos, hit.point.y, zPos)) > 1.5f;
+            
             // The dice can move only above the table, so that the player does not release the dice outside the table
-            _selectedRigidbody.transform.position = new Vector3(xPos, 3, zPos);
+            _selectedRigidbody.transform.position = new Vector3(xPos, _selectedRigidbody.transform.position.y, zPos);
 
             // The history of mouse movement over the last 15 frames,
             // which can then be used to give the throw force and direction and torque
             if (_dragTracking.Count > 15) _dragTracking.RemoveAt(0);
-            _dragTracking.Add(new Vector3(hit.point.x, 3, hit.point.z));
+            _dragTracking.Add(new Vector3(hit.point.x, dicePosY, hit.point.z));
         }
     }
     
     private void Throw()
     {
         if (_selectedRigidbody == null) return;
+        Cursor.visible = true;
+        
+        _diceMoveTween.Kill();
 
         _selectedRigidbody.useGravity = true;
         _selectedRigidbody.freezeRotation = false;
